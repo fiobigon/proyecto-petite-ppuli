74bae3c089ff188bb90e8d58f326af8a6d00f562
Fiorella Bigongiari <fiobigongiari@gmail.com>
2023-05-13 10:28:19 -0300

    Arreglos

---
+++ JavaScript.js
@@ -0,0 +1,7 @@
+const nombre = "Fiorella"
+const apellido = "Bigongiari"
+let edad = 25
+let pasatiempos = ["Leer", "ver peliculas", "tocar música", "jugar con mi perro"]
+let enPareja = false
+let soltera = true
+
